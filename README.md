0953bdc9c47116c5d926353bea3b77c37fa59bc6
gibbz00 <gabrielhansson00@gmail.com>
2023-11-24 21:38:41 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+1. Download repository:
+```bash
+git clone https://github.com/gibbz00/insyn && cd insyn
+```
+
+2. Install python dependency manager:
+```bash
+brew install poetry
+```
+
+3. Install project dependencies:
+```bash
+poetry install
+```
+
+4. Run main script:
+```bash
+poetry run python main.py
+```
+
+Output produced to `output.csv`.
+
+To and from dates can be adjusted in `main.py` accordingly.
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from swedish_market_insights.inside_trades import InsideTradesAPI
 from datetime import date
 
-recent_inside_trades = InsideTradesAPI.get_trades_by_transaction_date(from_date=date(2023, 11, 20),
-    to_date=date(2023, 11, 24))
+from_date = date(2023, 11, 20)
+to_date = date(2023, 11, 24)
 
-print(recent_inside_trades)
-
+recent_inside_trades = InsideTradesAPI.get_trades_by_transaction_date(from_date, to_date)
 recent_inside_trades = recent_inside_trades.sort_values(by='Issuer')
-
-print(recent_inside_trades)
-
 recent_inside_trades.to_csv('output.csv')
 
